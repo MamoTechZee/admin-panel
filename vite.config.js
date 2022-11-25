12b15c27136cf29f8af1a006a6b5186e2968b079
Zeeshan Chaudhary <zeeshan.stack26@gmail.com>
2022-11-25 21:04:22 +0530

    add element plus

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import ElementPlus from 'unplugin-element-plus/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import importElementPlus from 'vite-plugin-element-plus'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
   vue(),
-  importElementPlus(),
   ElementPlus({
     importStyle: 'css',
     useSource: true
