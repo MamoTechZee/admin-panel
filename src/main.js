7ea1e9e36e83df9abca9fa8b55bd4bbd36f8fba4
Zeeshan Chaudhary <zeeshan.stack26@gmail.com>
2022-11-25 20:25:34 +0530

    minor changes again

--- src/main.js
+++ src/main.js
@@ -2,5 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
-import installElementPlus from './plugins/element'
-createApp(App).use(router).use(installElementPlus).mount('#app')
+createApp(App).use(router).mount('#app')
