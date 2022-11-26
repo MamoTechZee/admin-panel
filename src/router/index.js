87a63c46246416a9281316643df5b7339968f5d3
Zeeshan Chaudhary <zeeshan.stack26@gmail.com>
2022-11-26 17:50:48 +0530

    update components

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,29 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-function guardMyroute(to, from, next)
-{
- var isAuthenticated= false;
-//this is just an example. You will have to find a better or 
-// centralised way to handle you localstorage data handling 
-if(localStorage.getItem('LoggedUser'))
-  isAuthenticated = true;
- else
-  isAuthenticated= false;
- if(isAuthenticated) 
- {
-  next(); // allow to enter route
- } 
- else
- {
-  next('/login'); // go to '/login';
- }
-}
+// function guardMyroute(to, from, next)
+// {
+//  var isAuthenticated= false;
+// //this is just an example. You will have to find a better or 
+// // centralised way to handle you localstorage data handling 
+// if(localStorage.getItem('LoggedUser'))
+//   isAuthenticated = true;
+//  else
+//   isAuthenticated= false;
+//  if(isAuthenticated) 
+//  {
+//   next(); // allow to enter route
+//  } 
+//  else
+//  {
+//   next('/login'); // go to '/login';
+//  }
+// }
 
 const routes = [
   {
     path: "/",
     component: ()=>import('../views/Admin.vue'),
-    beforeEnter : guardMyroute,
+    // beforeEnter : guardMyroute,
     children: [
       {
         path: '/',
